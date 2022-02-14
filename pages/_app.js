113db3ba38578ed3c37c272f9903fae7346805bd
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2022-02-14 05:43:35 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,30 @@
 import '/styles/globals.css'
-import Script from 'next/script';
+// import Script from 'next/script';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script 
-        strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=G-P5D2FYMTJD`}
+//       <Script 
+//         strategy='lazyOnload'
+//         src={`https://www.googletagmanager.com/gtag/js?id=G-P5D2FYMTJD`}
+//       />
+//       <Script strategy='lazyOnload'>
+//         {`
+//           window.dataLayer = window.dataLayer || [];
+//           function gtag(){dataLayer.push(arguments);}
+//           gtag('js', new Date());
+//           gtag('config', 'G-P5D2FYMTJD', {
+//             page_path: window.location.pathname,
+//           });
+//         `}
+//       </Script>
+    
+      <NextSeo
+        title='This is Next JS tutorial'
+        titleTemplate='Javohir Hakimov'
+        description='This is just test!!!'
       />
-      <Script strategy='lazyOnload'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-P5D2FYMTJD', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
       <Component {...pageProps} />
     </>
   )
